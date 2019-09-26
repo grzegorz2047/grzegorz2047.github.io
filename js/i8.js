61904cc37d5f686ed2bb67d8667f1807552e1e89
Grzegorz <grzegorz2047@gmail.com>
2019-09-27 00:19:07 +0200

    Zmiana strony na prostszą

---
+++ js/i8.js
@@ -0,0 +1,6 @@
+websiteDataJson = '{'+
+    '"mainTitle" : "Java, pluginy i inne takie tam pliki serwerowe do Minikrafta",'+
+    '"serverFilesHeader" : "Zacznijmy, do plików serwerowych",'+
+    '"serverFilesDescription" : "Generalnie, to trochę się tych plików serwerowych uzbierało. Z tego też powodu wrzuciłem wszystkie wersje na hosting, gdzie każda wersja ma swój folder. Jest tam trochę tych konfiguracji pluginów, więc na pewno sobie coś znajdziesz :)"'+
+    '}';
+    
