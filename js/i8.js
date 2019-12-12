9f10184eba7f95274d66be4f018166b5994eded8
Grzegorz <grzegorz2047@gmail.com>
2019-12-12 23:07:12 +0100

    Update i8.js

--- js/i8.js
+++ js/i8.js
@@ -1,6 +1,6 @@
 websiteDataJson = '{'+
     '"mainTitle" : "Java, pluginy i inne takie tam pliki serwerowe do Minikrafta",'+
     '"serverFilesHeader" : "Zacznijmy, do plików serwerowych",'+
-    '"serverFilesDescription" : "Generalnie, to trochę się tych plików serwerowych uzbierało. Z tego też powodu wrzuciłem wszystkie wersje na hosting, gdzie każda wersja ma swój folder. Jest tam trochę tych konfiguracji pluginów, więc na pewno sobie coś znajdziesz :)"'+
+    '"serverFilesDescription" : "Generalnie, to trochę się tych plików serwerowych uzbierało. Z tego też powodu wrzuciłem wszystkie wersje na hosting, gdzie każda wersja ma swój folder. Jest tam trochę tych konfiguracji pluginów, więc na pewno sobie coś znajdziesz :) <br> <center><h2>Aktualna wersja plików serwerowych 1.15</h2></center>""'+
     '}';
     
